e95f700239ae981c3d0905d4f7f1d6b47f4ee025
lastry <lastrycoding@gmail.com>
2020-07-23 02:53:29 +0100

    fix requests

--- api/common/query.js
+++ api/common/query.js
@@ -1,23 +1,27 @@
 const { connection } = require("../../utils/dbConnection");
 
 exports.query = async (Query) => {
-  let response = {};
-  await connection.query(Query, (err, rows) => {
-    if (err) {
-      console.error(err);
-      response = {
-        status: 500,
-        message: err,
-      };
-    } else {
-      console.log("test");
-      response = {
-        status: 200,
-        message: rows,
-      };
-    }
-    console.log(rows.length);
+  let promise = new Promise(async (resolve, reject) => {
+    await connection.query(Query, (err, rows) => {
+      let response = {};
+      if (err) {
+        console.error(err);
+        response = {
+          status: 500,
+          message: err,
+        };
+      } else {
+        console.log("test");
+        response = {
+          status: 200,
+          message: rows,
+        };
+      }
+      resolve(response);
+    });
   });
-  console.log(response);
-  return response;
+  let result = await promise.then((value) => {
+    return value;
+  });
+  return result;
 };
